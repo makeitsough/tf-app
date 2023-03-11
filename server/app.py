f712382e11603953a95e1bcd94a4de23465d98db
Paul Schofield <paul.schofield.us@gmail.com>
2023-03-11 15:51:59 -0500

    connected BE & FE

--- server/app.py
+++ server/app.py
@@ -1,7 +1,12 @@
-from flask import Flask
+from flask import Flask, jsonify
+from flask_cors import CORS
 
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return '<p>Hello, World!</p>'
+
+@app.route('/members')
+def members():
+    return {'members': ['John', 'Paul', 'George', 'Ringo']}
+
+if __name__ == '__main__':
+    app.run(debug=True)
